c8374b07fba5d7da51d6efc8a87e4d9cfd4628c5
Tom Forster <tom.forster@mpec.co.uk>
2018-03-16 17:26:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var Skill = /** @class */ (function () {
+    function Skill(name, description, baseCost, stacks, prerequisites) {
+        if (stacks === void 0) { stacks = false; }
+        if (prerequisites === void 0) { prerequisites = []; }
+        this.count = 0;
+        this.name = name;
+        this.description = description;
+        this.baseCost = baseCost;
+        this.hasPrerequisites = prerequisites.length > 0;
+        this.prerequisites = prerequisites;
+        this.stacks = stacks;
+    }
+    Skill.prototype.cost = function () {
+        return this.stacks ? this.baseCost + this.count : this.baseCost;
+    };
+    Skill.prototype.pointsSpent = function () {
+        if (!this.stacks)
+            return this.baseCost;
+        var points = 0;
+        for (var i = 0; i < this.count; i++) {
+            points += this.baseCost + i;
+        }
+        return points;
+    };
+    return Skill;
+}());
+var combatSkills = [
+    new Skill("Toughness", "Grants +1 locational body hit per rank.", 2, true),
+    new Skill("Resilience", "Your death count is extended by 100 seconds per rank.", 1, true),
+    new Skill("Dual Weapons Training", "You can fight with two weapons simultaneously.", 2),
+    new Skill("Shield Training", "You can use shields to block attacks.", 2),
+    new Skill("Large Melee Weapon Training", "You can use large melee weapons such as halberds and great weapons.", 2),
+    new Skill("Projectile Weapon Training", "You can use a projectile weapon like a crossbow or bow.", 4),
+    new Skill("Medium Energy Weapon Training", "You can use a medium energy weapon (NERF Dart gun).", 2)
+];
+combatSkills.push(new Skill("Heavy Energy Weapon Training", "You can use a heavy energy weapon (NERF Dart gun).", 2, undefined, [combatSkills[combatSkills.length - 1]]), new Skill("Medium Armour Training", "You can wear medium armour effectively (2 locational hits).", 2));
+combatSkills.push(new Skill("Heavy Armour Training", "You can wear heavy armour effectively (3 locational hits).", 2, undefined, [combatSkills[combatSkills.length - 1]]), new Skill("Steel Nerves", "Reduce the duration of FREEZE calls that affect you by 5 seconds.", 2, true), new Skill("Steel Head", " Reduce the duration of KNOCKOUT calls that affect you by 5 seconds.", 2, true), new Skill("Steel Viscera", "Double the progression time of toxins & diseases that affect you.", 2, true));
+var MainController = /** @class */ (function () {
+    function MainController() {
+        this.totalPoints = 10;
+        this.points = 10;
+        this.combatSkills = combatSkills;
+        this.selectedSkills = [];
+    }
+    MainController.prototype.handleAddSkillButtonClick = function (skill) {
+        if ((skill.stacks || skill.count === 0) && skill.cost() <= this.points) {
+            skill.count++;
+            this.updateSelected();
+        }
+    };
+    MainController.prototype.handleRemoveSkillButtonClick = function (skill) {
+        skill.count--;
+        this.updateSelected();
+    };
+    MainController.prototype.updateSelected = function () {
+        this.selectedSkills = this.combatSkills.filter(function (skill) { return skill.count > 0; });
+        this.points = this.totalPoints - this.selectedSkills.reduce(function (total, next) { return total + next.pointsSpent(); }, 0);
+        if (this.points < 0)
+            throw "ERROR";
+    };
+    MainController.prototype.isDisabled = function (skill) {
+        if (!skill.hasPrerequisites)
+            return false;
+        else {
+            return !!skill.prerequisites.find(function (s) { return s.count < 1; });
+        }
+    };
+    return MainController;
+}());
+var angular;
+var module = angular.module("calc", []);
+module.controller("MainController", MainController);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,162 @@
+import {IController} from "angular";
+
+class Skill
+{
+    name:string;
+    baseCost:number;
+    hasPrerequisites:boolean;
+    stacks:boolean;
+    description:string;
+    count:number = 0;
+    prerequisites:Skill[];
+    
+    constructor(name:string, description:string, baseCost:number, stacks:boolean = false, prerequisites:Skill[] = [])
+    {
+        this.name = name;
+        this.description = description;
+        this.baseCost = baseCost;
+        this.hasPrerequisites = prerequisites.length > 0;
+        this.prerequisites = prerequisites;
+        this.stacks = stacks;
+    }
+    
+    cost(){
+        return this.stacks ? this.baseCost + this.count : this.baseCost;
+    }
+    
+    pointsSpent()
+    {
+        if(!this.stacks) return this.baseCost;
+        
+        let points = 0;
+        
+        for(let i = 0; i < this.count; i++)
+        {
+            points += this.baseCost + i;
+        }
+        
+        return points;
+    }
+}
+
+const combatSkills:Skill[] = [
+    new Skill(
+        "Toughness",
+         "Grants +1 locational body hit per rank.",
+         2,
+        true
+    ),
+    new Skill(
+        "Resilience",
+         "Your death count is extended by 100 seconds per rank.",
+         1,
+         true
+    ),
+    new Skill(
+        "Dual Weapons Training",
+         "You can fight with two weapons simultaneously.",
+         2
+    ),
+    new Skill(
+        "Shield Training",
+         "You can use shields to block attacks.",
+         2
+    ),
+    new Skill(
+        "Large Melee Weapon Training",
+         "You can use large melee weapons such as halberds and great weapons.",
+         2
+    ),
+    new Skill(
+        "Projectile Weapon Training",
+         "You can use a projectile weapon like a crossbow or bow.",
+         4
+    ),
+    new Skill(
+        "Medium Energy Weapon Training",
+         "You can use a medium energy weapon (NERF Dart gun).",
+         2
+    )
+];
+combatSkills.push(
+    new Skill(
+        "Heavy Energy Weapon Training",
+         "You can use a heavy energy weapon (NERF Dart gun).",
+         2,
+        undefined,
+        [combatSkills[combatSkills.length - 1]]
+    ),
+
+    new Skill(
+        "Medium Armour Training",
+         "You can wear medium armour effectively (2 locational hits).",
+         2
+    ));
+combatSkills.push(
+    new Skill(
+        "Heavy Armour Training",
+         "You can wear heavy armour effectively (3 locational hits).",
+         2,
+        undefined,
+        [combatSkills[combatSkills.length - 1]]
+    ),
+    new Skill(
+        "Steel Nerves",
+         "Reduce the duration of FREEZE calls that affect you by 5 seconds.",
+         2,
+         true
+    ),
+    new Skill(
+        "Steel Head",
+         " Reduce the duration of KNOCKOUT calls that affect you by 5 seconds.",
+         2,
+         true
+    ),
+    new Skill(
+        "Steel Viscera",
+         "Double the progression time of toxins & diseases that affect you.",
+         2,
+         true
+    ));
+
+class MainController implements IController
+{
+    totalPoints = 10;
+    points = 10;
+    combatSkills:Skill[] = combatSkills;
+    selectedSkills:Skill[] = [];
+    
+    handleAddSkillButtonClick(skill:Skill)
+    {
+        if((skill.stacks || skill.count === 0) && skill.cost() <= this.points)
+        {
+            skill.count++;
+            this.updateSelected();
+        }
+    }
+    
+    handleRemoveSkillButtonClick(skill:Skill)
+    {
+        skill.count--;
+        this.updateSelected();
+    }
+    
+    updateSelected()
+    {
+        this.selectedSkills = this.combatSkills.filter(skill => skill.count > 0);
+        this.points = this.totalPoints - this.selectedSkills.reduce((total, next) => total + next.pointsSpent(), 0);
+        if(this.points < 0) throw "ERROR";
+    }
+    
+    isDisabled(skill:Skill)
+    {
+        if(!skill.hasPrerequisites) return false;
+        else {
+             return !!skill.prerequisites.find(s => s.count < 1);
+        }
+    }
+}
+
+let angular:any;
+const module = angular.module("calc", []);
+module.controller("MainController", MainController);
